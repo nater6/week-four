967e687d6fe797c40a6c5ad032c022644f4c80c6
Your Name <you@example.com>
2022-01-11 15:54:49 +0000

    Back

---
+++ listpushfront.go
@@ -0,0 +1,25 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = n
+	} else {
+		f := l.Head
+		for f != nil {
+			f.Next = f
+		}
+		l.Head = n
+	}
+}
