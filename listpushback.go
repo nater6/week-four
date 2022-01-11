da40b937b58c26c6d66b1f2f8f8c84f0c2d48483
nater68 <nater68@noreply.git.learn.01founders.co>
2022-01-11 16:21:22 +0000

    Update 'listpushback.go'

--- listpushback.go
+++ listpushback.go
@@ -1,14 +1,14 @@
 package piscine
 
-//type NodeL struct {
-//	Data interface{}
-//	Next *NodeL
-//}
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
 
-//type List struct {
-//	Head *NodeL
-//	Tail *NodeL
-//}
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
 
 func ListPushBack(l *List, data interface{}) {
 	n := &NodeL{Data: data}
