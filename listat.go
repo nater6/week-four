d15c549432c6a405db5205d7b1a6ed3d16fa8955
Your Name <you@example.com>
2022-01-12 12:34:35 +0000

    a

--- listat.go
+++ listat.go
@@ -2,17 +2,14 @@ package piscine
 
 func ListAt(l *NodeL, pos int) *NodeL {
 	counter := 0
-	if pos == 0 {
-		return l
-	} else {
-
-		// Secnd Part
-
-		f := l
-		for counter != pos {
-			f = f.Next
-			counter++
-		}
+	f := l
+	for counter != pos {
+		f = f.Next
+		counter++
+	}
+	if counter == pos {
 		return f
 	}
+
+	return nil
 }
