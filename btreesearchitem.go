67a506eda47cda735c20fd10fb4fe5a9ff8f2618
Your Name <you@example.com>
2022-01-13 16:31:46 +0000

    a

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -3,13 +3,9 @@ package piscine
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil || root.Data == elem {
 		return root
-	} else {
-		if elem < root.Data {
-			// go left
-			return BTreeSearchItem(root.Left, elem)
-		} else {
-			// Go right
-			return BTreeSearchItem(root.Right, elem)
-		}
 	}
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
+	}
+	return BTreeSearchItem(root.Right, elem)
 }
