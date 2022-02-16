d150ffe8a4d10caa56c012a344c4d590eca3d726
nater68 <nater68@noreply.git.learn.01founders.co>
2022-02-16 13:53:27 +0000

    Delete 'btreeapplyinorder.go'

--- btreeapplyinorder.go
+++
@@ -1,9 +0,0 @@
-package piscine
-
-func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
-	if root != nil {
-		BTreeApplyInorder(root.Left, f)
-		f(root.Data)
-		BTreeApplyInorder(root.Right, f)
-	}
-}
