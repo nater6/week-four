084c700a749c7e79767d88fad3f5757e89b5cc7e
Your Name <you@example.com>
2022-02-16 16:00:44 +0000

    Reverse

---
+++ listreverse.go
@@ -0,0 +1,21 @@
+package piscine
+
+func ListReverse(l *List) {
+	cur := l.Head
+	var nex *NodeL
+	var pre *NodeL
+
+	for cur != nil {
+		nex = cur.Next
+		cur.Next = pre
+		pre = cur
+
+		if nex != nil {
+			cur = nex
+		} else {
+			l.Head = cur
+			cur = nex
+		}
+
+	}
+}
