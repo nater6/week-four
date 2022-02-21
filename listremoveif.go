8897eb4d69759e7250486df9aa60d92a3499fb81
Your Name <you@example.com>
2022-02-21 18:18:03 +0000

    Finally

--- listremoveif.go
+++ listremoveif.go
@@ -9,14 +9,16 @@ func ListRemoveIf(l *List, data_ref interface{}) {
 		return
 	}
 
-	x := l.Head
+	curr := l.Head
+	var prev *NodeL
 
-	for x != nil {
-
-		if x.Next.Data == data_ref {
-			x.Next = x.Next.Next
+	for curr != nil {
+		if curr.Data != data_ref {
+			prev = curr
+			curr = curr.Next
+		} else {
+			prev.Next = curr.Next
+			curr = curr.Next
 		}
-		x = x.Next
-
 	}
 }
