41beb9597271f15ea18703f9c1d594f67cd0d036
Your Name <you@example.com>
2022-02-24 13:26:38 +0000

    With Parent Adder

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -7,9 +7,15 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 
 	if data < root.Data {
 		// If data is less then parent go left
+		if root.Left != nil {
+			root.Left.Parent = root
+		}
 		root.Left = BTreeInsertData(root.Left, data)
 	} else {
 		// If data is greater or equal to the parent then go right
+		if root.Right != nil {
+			root.Right.Parent = root
+		}
 		root.Right = BTreeInsertData(root.Right, data)
 	}
 	return root
